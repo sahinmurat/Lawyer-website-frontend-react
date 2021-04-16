1c14a029e94ec13b84f8135b5b1727a899bf3c82
Murat Sahin <yazilimogren1111@gmail.com>
2021-04-16 21:46:20 +0200

    About component is created, still on work

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "../components/About/About";
 import Connection from "../components/Connection/Connection";
 import Header from "../components/Header/Header";
 import Navbar from "../components/Navbar/Navbar";
@@ -11,6 +12,7 @@ const AppRouter = () => {
                <Route exact path='/' component={Header} />
            </Switch>
            <Connection/>
+           <About/>
        </Router>
     )
 }
