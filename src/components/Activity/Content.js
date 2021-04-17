8250f0943f3cee08e363d8c20503427847eb15bb
Murat Sahin <yazilimogren1111@gmail.com>
2021-04-17 22:38:06 +0200

    connect button's style changed, in activity component contex added

---
+++ src/components/Activity/Content.js
@@ -0,0 +1,244 @@
+// export const content =
+
+// " KİŞİSEL VERİLERİN KORUNMASI HUKUKU
+// Kişisel Verilerin Korunması Hukuku Nedir?
+// Öncelikle kişisel veri kavramından bahsetmek gerekirse; kimliği belirli ya da belirlenebilir nitelikteki bir kişiye ait her türlü bilginin kişisel veri olduğu kabul edilir.
+// Günümüz dünyasında gerek sosyal ve ekonomik ilişkilerin artması gerekse dijitalleşmenin artması gibi nedenlerle kişisel veriler özel kurumlar veya kamu kurumları tarafından yoğun biçimde işlenmektedir. İşbu durumun kontrol altına alınması amacıyla dünyada birçok ülke düzenlenecek bir mevzuat ile kişisel verileri koruma ihtiyacı hissetmiş ve Kişisel Verilerin Korunması Hukuku ortaya çıkmıştır.
+// Kişisel Verilerin Korunması Hukuku Bakımından Hizmetlerimiz Nelerdir? 
+// 6698 sayılı Kişisel Verilerin Korunması Kanunu 7 Nisan 2016 tarihinde Resmî Gazetede yayınlanmak suretiyle yürürlüğe girmiştir. Kanun’un getirdiği yükümlülüklere uyumlu hale gelmek adına; Türkiye’nin önde gelen çözüm ortakları ile iş birliği çerçevesinde Kişisel Verilerin Korunması Kanunu’na uyumluluk projeleri yürütmekteyiz. Bu uyumluluk projeleri kapsamında gerçekleştirdiğimiz aşamalar ise şu şekildedir:
+// •	Boşluk analizi
+// •	Kişisel veri envanterinin çıkarılması
+// •	Farkındalık eğitimi
+// •	Uyum danışmanlığı
+// Kişisel Verilerin Korunması Kanuna uyumluluk projesinin tamamlanmasından sonra Kanunun uygulanması kapsamında kişisel verilerin korunması eğitimlerinin güncellenmesi, ortaya çıkan ihtilaflarda gündeme gelebilecek idari para cezaları ve tazminat taleplerine itiraz, dava takipleri ile müvekkillerin KVKK kapsamında müşterileriyle veya iş ortaklarıyla akdetmeleri gereken sözleşmelerin hazırlanması ve revize edilmesi Erol Hukuk & Danışmanlık tarafından güncel gelişmeler takip edilerek yürütülmektedir.
+
+
+
+
+// TIP HUKUKU
+// Tıp Hukuku Nedir?
+// Sağlık Hukuku’nun bir alt dalı olarak, tıbbın uygulanmasından kaynaklanan sağlık personelinin hak ve yükümlülükleri, yasal sorumluluğu, hasta hakları, ilaç hukuku, medikal hukuk gibi konuları ele alan hukuk dalıdır. Tıp Hukuku, tıbbi müdahalenin hukuku olarak da nitelendirilebilir ve bu bakımdan tıbbi müdahaleyi hekimin veya diğer sağlık çalışanlarının yapması arasında bir ayrım yapılmaz.
+// Malpraktis Nedir?
+// Malpraktis/doktor hatası; hekimin veya tıbbi müdahale merkezi olan sağlık kuruluşlarının bilgisizlik, deneyimsizlik, ilgisizlik, bu ilgisizlikten kaynaklanan yanlış teşhis ve yanlış tedavi, eksik tedavi sonucunda hastanın mevcut hastalığında bir sapma görülmesi, yeni bir hastalığa sebebiyet verilmesi halidir. Malpraktis sonucunda cezai ve idari yaptırımların yanında maddi ve manevi tazminat yükümlülüğü de doğacaktır. 
+// Tıp Hukuku Bakımından Hizmetlerimiz Nelerdir?
+// Erol Hukuk & Danışmanlık, sağlık hukuku bağlamında doktor ve sağlık çalışanlarına, hasta ve hasta yakınlarına hukuki çözüm önerileri ve danışmanlık hizmetleri sağlamaktadır. Bu kapsam;
+// •	Hekim, sağlık çalışanları ve hasta haklarının korunması
+// •	Malpraktis (doktor veya sağlık personeli hataları) kaynaklı hukuki süreçlerinin başlatılması ve takibi
+// •	Tıp ve ilaç hukukundan doğan akdi ve idari uyuşmazlıkların çözüm davaları ve takibi
+// •	Hekimlerin mesleki, cezai, tazminat, idari ve mesleki sorumluluklarından doğabilecek dava süreçlerinin takibi
+// •	Sigorta uyuşmazlıklarının giderilmesi
+// •	Ölüm, yaralanma, uzuv kaybı, maluliyet gibi birçok zarar sebebiyle hastane ve doktor aleyhine sağlık tazminat davalarının açılması ve takibinde profesyonel şekilde avukatlık hizmeti verilmektedir. 
+
+
+
+
+
+// ŞİRKETLER HUKUKU
+// Şirketler Hukuku Nedir?
+// Ticari faaliyetlerde bulunma olanaklarına sahip olan şirketlerin kurulması, birleşmesi, devir edilmesi, tasfiyesi ve bölünmesi vb. konularına dair hukuki ilişkileri inceleyen bir hukuk dalıdır.
+// Şirketler Hukuku Bakımından Hizmetlerimiz Nelerdir?
+// Erol Hukuk & Danışmanlık, Şirketler Hukuku kapsamında;
+// •	Şirket birleşmeleri, bölünme ve devralma danışmanlığı
+// •	Şirket kuruluşu ve şirketin tescili
+// •	Şirketin taraf olduğu davaların yürütülmesi
+// •	Sözleşmelerin uyumlu hale getirilmesi
+// •	Esas sözleşmelerin hazırlanması ve güncellenmesi
+// •	Şirket alacaklarının yasal yollarla tahsili
+// •	Şirket yapılandırmaları
+// •	Haksız rekabetten doğan uyuşmazlıklar ve çözümü
+// •	Ortaklar sözleşmesi ve ortaklar arası ilişkilerin kararlaştırılması
+// •	Sermaye işlemlerinin denetim ve yürütülmesinde kaliteli ve yenilikçi çözümler ile profesyonel hizmet desteği vermektedir.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DENİZ TİCARETİ HUKUKU
+
+// Deniz Ticareti Hukuku Nedir?
+
+// Deniz Ticareti Hukuku, gemiler ve gemilerin denizde seyrüseferi ile ilgili hukuki ilişkileri düzenleyen kuralları içeren özel hukuk dalıdır ve ticari nitelikteki ilişkileri düzenleyen kuralları içermektedir. Bu özelliği itibarı ile, Deniz Ticaret Hukuku, Ticaret Hukuku’nun bir dalını oluşturur. 
+
+// Deniz Ticareti Hukuku Bakımından Hizmetlerimiz Nelerdir?
+
+// Erol Hukuk & Danışmanlık, Deniz Ticareti Hukuku kapsamında;
+
+// •  Çatma
+// •  Seferden men kararlarının alınması
+// •  Müşterek avarya
+// •  Yakıt alacaklarının tahsili
+// •  Gemi adamı alacaklarının tahsili
+// •  Kanuni rehin hakkı kararlarının icrası
+// •  Hapis hakkı tesisi
+// •  Charterparty metinlerinin hazırlanması
+// •  Laytime ve Demuraj sürelerinin hesaplanmasından kaynaklanan hukuki uyuşmazlıklar
+// •  Time-Charter kira bedellerinin tahsilinde geç veya hiç ödeme yapmama şeklinde ortaya çıkan   uyuşmazlıkların çözümü
+// •  Gemi tescil ve terkin işlemleri
+// •  Deniz hukukundaki Mortgage işlemleri
+// •  Gemi sicil işlemlerinin closing aşamasında tarafların temsili
+// •  Deniz hukuku alanında sözleşmelerin hazırlanması
+// •  Deniz Sigorta Hukuku alanında çözüm odaklı ve etkin yöntemler ile hukuk hizmeti vermektedir.
+
+
+
+
+
+
+// FİKRİ VE SINAİ MÜLKİYET HUKUKU
+
+// Fikri ve Sınai Mülkiyet Hukuku Nedir?
+// İktisadi değer taşıyan fikrî emek ürünlerini koruyan bir hukuk dalıdır.
+// Fikri ve Sınai Mülkiyet hakları, bir işletmenin ürünleri, yöntemleri (usulleri), dokümanları, görselleri, kimlik unsurları, sanat eserleri, vb.'ne ilişkin tüm özgün tasarımları ve gizli ya da açık bilgileri (gayri maddi varlıkları) ile ilgili sahip olduğu, kullanma, ürüne dönüştürme, dağıtma, yayma, satma gibi çeşitli mahiyetteki hakları olarak da tanımlamak mümkündür.
+// Fikri ve Sınai Mülkiyet Hukuku, aşağıda yer alan hakları koruma altına almaktadır. 
+// •	Patent Hakkı
+// •	Faydalı Model Hakkı
+// •	Markanın Korunması Hakkı
+// •	Endüstriyel Tasarım Hakkı
+// •	Coğrafi İşaretin Korunması Hakkı 
+// Fikri ve Sınai Mülkiyet Kanunu Kapsamında Hizmetlerimiz Nelerdir?
+// Erol Hukuk & Danışmanlık, Sınai Mülkiyet Hukuku Kapsamında;
+// •	Marka, patent, endüstriyel tasarımların araştırılması, tescili, korunması ve devredilmesi işlemlerinin yürütülmesi
+// •	Telif hakları ve ticari sınırların korunması
+// •	Sınai hakların Türk Patent Enstitüsü nezdinde takibi ile dava ve uyuşmazlıkların çözüme kavuşturulması
+// •	Lisanslama görüşmeleri danışmanlığı ve lisans sözleşmelerinin hazırlanması
+// •	Fikri Sınai Haklar Uyuşmazlıklarından doğan davaların takibi konusunda çözüm odaklı ve etkin yöntemler ile avukatlık hizmeti vermektedir.
+
+
+
+
+
+
+
+// SÖZLEŞMELER HUKUKU 
+
+// Sözleşmeler Hukuku Nedir?
+
+// Öncelikle sözleşme; birden fazla gerçek veya tüzel kişinin birbirine karşı haklar kazandığı veya yükümlülük altına girdikleri anlaşmalardır. Hukukun neredeyse her dalını kapsayan sözleşmeler hukuku özellikle Tüketici Hukuku, İş Hukuku, Ticaret Hukuku, Kişisel Verilerin Korunması Hukuku, Aile Hukuku, Miras Hukuku gibi alanlarda karşımıza çıkmaktadır.
+// Anayasa ile güvence altına alınmış olan “sözleşme yapma serbestisi” nedeniyle taraflar; özel kanun hükümlerindeki sözleşme tipleri ile sınırlı tutulmamış, genel hukuk kurallarına aykırı olmadığı sürece diledikleri alanda iradelerini ortaya koyma konusunda özgür kılınmıştır. Bu durum, kanunlarda yer alan sözleşme tiplerinin dışında sözleşmelerin ortaya çıkmasına ve Sözleşmeler Hukuku’nun yeni bir hukuk dalı olarak doğmasına neden olmuştur.
+
+// Sözleşmeler Hukuku Kapsamında Hizmetlerimiz Nelerdir?
+
+// Erol Hukuk & Danışmanlık, Sözleşmeler Hukuku Kapsamında;
+
+// •	Ön sözleşme, 
+// •	Hazırlık, müzakere ve imza aşamalarında temsil, 
+// •	Olası ihlal ve zararların önüne geçilebilmesi adına cezai şartların belirlenmesi,
+// •	Franchising (bayilik) veya sponsorluk gibi kanunlarda düzenlenmeyen sözleşmelerin genel çerçeve ve davranışa uygunluğunun denetlenmesi 
+// •	Devre tatil, kat karşılığı inşaat sözleşmesi veya hastane kabul sözleşmesi gibi karma işlemlerde de şekil ve esasa ilişkin şartların tespiti, 
+// •	Kira sözleşmesi; sözleşme gereği bildirim ve ihtar gibi işlemler ile sözleşmeyi sonlandırma maksatlı fesih ve dönme beyanlarının karşı tarafa gönderilmesi, 
+// •	Sözleşme ihlali hallerinde ise ifaya zorlama, dönme, fesih, tazminat, bedel veya eşya iadesi ve cezai şartın uygulanması taleplerini sulh olma, arabuluculuk, tahkim, dava ve icra yollarına başvurarak ileri sürme işlemlerinde kaliteli ve yenilikçi çözümler ile profesyonel hizmet desteği vermektedir.
+
+
+
+
+
+
+// VERGİ HUKUKU 
+// Vergi Hukuku Nedir?
+// Devletler; sahip oldukları maddi kaynaklar ile kamu hizmetlerini yerine getirirler. Vergi Hukuku bu kaynakların nasıl elde edileceğini ve yasal süreçlerini konu alan hukuk dalıdır. Bir başka ifade ile Vergi Hukuku; devlet ve vergi mükellefleri arasındaki vergi ilişkisinden doğan hak ve ödevleri ele alır.
+// Vergi Hukuku Kapsamında Hizmetlerimiz Nelerdir?
+// Erol Hukuk & Danışmanlık, Vergi Hukuku kapsamında;
+// •	Tüm idari başvuru ve itirazların yapılması
+// •	Hukuka aykırı idari işlemlerin iptaline ilişkin dava süreçlerinin takibi
+// •	Dava süresince tüm uzlaşma işlemlerinin yürütülmesi
+// •	Vergi cezalarının iptali işlemleri ve düzeltme talepleri
+// •	İdari sözleşmelerden doğan tavaların takibi
+// •	Uyuşmazlık çözümleri ve dava takibi
+// •	Vergi suç ve cezaları hususunda kaliteli ve yenilikçi çözümler ile profesyonel hizmet desteği vermektedir.
+
+
+
+
+
+
+
+
+
+
+
+
+
+// İMAR HUKUKU
+
+// İmar Hukuku Nedir?
+
+// İmar hukuku herhangi bir arazi veya arsa üzerinde yapılması planlanan resmi veya özel yerleşim yerlerinin, amaçları ve çeşitlerine göre hangi niteliklere sahip olması gerektiğini düzenleyen kuralları kapsamaktadır. İmar hukuku tarafından düzelenmiş alanlara resmi veya özel amaçlarla yapılmış tüm bayındırlık fiilleri girmektedir. İmar hukukuyla elde edilmek istenen gaye, tüm yurtta çevre ve sağlık şartlarına uygun yapılaşmanın sağlanmasıdır.
+
+// İmar Hukuku Kapsamında Hizmetlerimiz Nelerdir?
+
+// Erol Hukuk & Danışmanlık, İmar Hukuku kapsamında;
+
+// •	Koruma Kurulları kararlarının iptali davaları
+// •	Tescil kararının iptali davaları
+// •	Doğal sit alanlarında yapılaşmaya ilişkin davalar
+// •	Doğal sit alanlarında madencilik faaliyetleri
+// •	Sit alanlarında derecelendirmeye dair uyuşmazlıklar
+// •	Arkeolojik sit alanlarında yapılaşmaya ilişkin davalar
+// •	Koruma Bölge Kurullarınca alınan kararlara idari itirazlar
+// •	Koruma Yüksek Kurulunun ilke kararlarının iptali davaları
+// •	Koruma Yüksek Kurulu ve Koruma Bölge Kurulları kararlarının iptali
+// •	Koruma amaçlı imar planlarına idari itiraz ve iptal davaları
+// •	İmar Planı değişiklileriyle ilgili idari davalar
+// •	Sit alanlarında (doğal, arkeolojik, kentsel, tarihi ve diğer karma sitler)  mülkiyeti bulunanların, taşınmaz kültür ve tabiat varlıklarının muafiyet ve istisnalardan yararlanmaları
+// •	Koruma altına alınan taşınmazlarla ilgili yapılacak her türlü uygulamalar, izinlerin alınmasına yönelik idari başvurular
+// •	Korunması gerekli taşınmaz kültür ve tabiat varlıkları ile koruma alanları ve sit alanlarında izinsiz müdahale ve fonksiyonu dışında kullanmaya karşı uygulanan cezai yaptırımlara karşı korunma
+// •	Tescilli taşınmaz kültür ve tabiat varlıklarının bulunduğu parseller ve bu varlıkların koruma alanlarında ruhsatsız, sit alanlarında imar planı hükümlerine aykırı idari ve cezai yaptırımlara karşı hukuki başvurular, gerekli izinlerin alınmasında hukuk danışmanlığı
+// •	Taşınmaz kültür ve tabiat varlıklarının maliklerine Bakanlıkça ve Mahalli idarelerce yapılacak yardımlara başvuru sürecinin yönetilmesi
+// •	Vakıf veya kısmen gerçek veya tüzel kişilerin mülkiyetinde olan korunması gereken kültür ve tabiat varlıkları ile koruma alanlarının kamulaştırılmasından kaynaklanan davalarda profesyonel ve çözüm odaklı hizmet anlayışı ile faaliyetlerini sürdürmektedir. 
+
+// TÜKETİCİ HUKUKU
+
+// Tüketici Hukuku Nedir?
+
+// Tüketicilerin ekonomik çıkarlarının yanı sıra sağlığını ve güvenliğini korumak adına ve yine tüketicinin zararlarını karşılamak, bunu da yasal çerçeve doğrultusunda gerçekleştirmek adına düzenlemelerde bulunan hukuk dalıdır.
+
+// Tüketici Hukuku Kapsamında Hizmetlerimiz Nelerdir?
+
+// Erol Hukuk & Danışmanlık, Tüketici Hukuku Kapsamında;
+
+// •	Tüketici Hakem Heyeti nezdinde yapılacak işlemler,
+// •	Tüketicinin şikayetlerine karşı hukuki çözüm yolları üretmek,
+// •	Satıcı ile alıcı arasında yapılacak sulh görüşmeleri ve görüşmelerin protokole bağlanması
+// •	Ayıplı mal ya da ayıplı hizmetten kaynaklanan uyuşmazlıkların çözümü
+// •	Tüketici ve Ticaret Mahkemelerinde görülen uyuşmazlıkların çözümünde kaliteli ve yenilikçi çözümler ile profesyonel avukatlık hizmeti vermektedir.
+
+
+
+
+
+
+
+
+
+
+// ELEKTRONİK TİCARET (E-TİCARET) HUKUKU 
+
+// Elektronik Ticaret Hukuku Nedir?
+
+// Elektronik Ticaret Hukuku, Bilişim Hukuku ile bağlantılı bir alan olup, başta online alışverişler olmak üzere e-ticaret kapsamına giren mevzuat ile hukuki konulara ilişkin bir hukuk disiplinidir.
+
+// ELEKTRONİK TİCARET (E-TİCARET) HUKUKU BAKIMINDAN HİZMETLERİMİZ NELERDİR?
+
+// Erol Hukuk & Danışmanlık, Elektronik Ticaret (E-Ticaret) Hukuku kapsamında;
+// •	Kuruluş öncesinde vergi yükümlülükleri, ödeme araç ve yöntemleri, marka, lisans gibi Fikri Mülkiyet konularını gözden geçirilmesi,
+// •	Gerek üyelik gerek mesafeli satış sözleşmelerinin iş modeline ve mevzuata uygun olarak düzenlenmesi,
+// •	Yasal düzenlemeden kaynaklanan bilgi verme yükümlülüklerinin yerine getirilmesi,
+// •	İşlem Rehberine yer verilmesi,
+// •	Cayma hakkı hususunda bilgilendirme,
+// •	Sipariş onayından önce ödenecek toplam bedelin ve sipariş özetinin gösterilmesi,
+// •	Sipariş sonrasında onay gönderilmesi,
+// •	Elektronik kayıtların saklanması,
+// •	Elektronik haberleşme için onayların alınması, elektronik pazarlama faaliyetlerinin düzenlemelere uygun hale getirilmesi,
+// •	Güven damgası sistemine dahil olunması,
+// •	Elektronik Ticaret Bilgi Sistemi (ETBİS)’ e kayıt konusunda çözüm odaklı ve etkin yöntemler ile avukatlık hizmeti vermektedir."
