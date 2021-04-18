faff6376b5d1c6c2f74a139c8c0bfba3d5d7dc8a
Murat Sahin <yazilimogren1111@gmail.com>
2021-04-18 02:28:00 +0200

    activity component styled

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -4,17 +4,18 @@ import './Activity.css'
 function Activity() {
     return (
         <div className='wrapper-all' >
-            <div className='header'>FAALİYET ALANLARI</div>
+            <h2 className='titel'>FAALİYET ALANLARI</h2>
             <div className='wrapper-titel' >
-                <div className='activity-titel' >ELEKTRONİK TİCARET (E-TİCARET) HUKUKU </div>
-                <div className='activity-titel' >TÜKETİCİ HUKUKU</div>
-                <div className='activity-titel' >İMAR HUKUKU</div>
-                <div className='activity-titel' >VERGİ HUKUKU </div>
-                <div className='activity-titel' >SÖZLEŞMELER HUKUKU </div>
-                <div className='activity-titel' >FİKRİ VE SINAİ MÜLKİYET HUKUKU</div>
-                <div className='activity-titel' >ŞİRKETLER HUKUKU</div>
-                <div className='activity-titel' >TIP HUKUKU</div>
-                <div className='activity-titel' >KİŞİSEL VERİLERİN KORUNMASI HUKUKU</div>
+                <div className='activity-titel elektro' >ELEKTRONİK TİCARET (E-TİCARET) HUKUKU </div>
+                <div className='activity-titel tüketici' >TÜKETİCİ HUKUKU</div>
+                <div className='activity-titel imar' >İMAR HUKUKU</div>
+                <div className='activity-titel vergi' >VERGİ HUKUKU </div>
+                <div className='activity-titel sözlesme' >SÖZLEŞMELER HUKUKU </div>
+                <div className='activity-titel fikir' >FİKRİ VE SINAİ MÜLKİYET HUKUKU</div>
+                <div className='activity-titel deniz' >DENİZ TİCARETİ HUKUKU</div>
+                <div className='activity-titel sirket' >ŞİRKETLER HUKUKU</div>
+                <div className='activity-titel tip' >TIP HUKUKU</div>
+                <div className='activity-titel kvkk' >KİŞİSEL VERİLERİN KORUNMASI</div>
             </div>
         </div>
     )
