ad6f7452d3a43b81b862daa4e4539cc2c85dc155
Murat Sahin <yazilimogren1111@gmail.com>
2021-05-30 23:52:52 +0200

    data from backend fetched

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
+
 
 function Blog() {
+    const [list , setList] = useState([])
+
+useEffect(()=>{
+    fetchData()
+}, [])
+
+const fetchData = async () => {
+    var result = await axios.get('https://emirback.herokuapp.com/list');
+    setList(result);
+    console.log(result);
+}
+
     return (
         <div>
            <h2> Blog  Page </h2> 
            <p>Last Content</p>
+           <p>{JSON.stringify(list)}</p>
         </div>
     )
 }
