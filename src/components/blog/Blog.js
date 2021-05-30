e7fe865db92a5ff5a2c1477b7d2a54ffd140bdf8
Murat Sahin <yazilimogren1111@gmail.com>
2021-05-31 00:09:00 +0200

    fetched data is tested

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -12,15 +12,19 @@ useEffect(()=>{
 
 const fetchData = async () => {
     var result = await axios.get('https://emirback.herokuapp.com/list');
-    setList(result);
-    console.log(result);
+    setList(result.data.results);
+    console.log('result.data', result.data.results);
 }
 
     return (
         <div>
            <h2> Blog  Page </h2> 
            <p>Last Content</p>
-           <p>{JSON.stringify(list)}</p>
+           <p> author : {list[0]?.author}</p>
+           <p>category : {list[0]?.category}</p>
+           <p>title : {list[0]?.title}</p>
+           <p>related image</p>
+           <img src={list[0]?.image} alt='content' />
         </div>
     )
 }
