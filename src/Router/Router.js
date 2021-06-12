151d041b6f6e5f7670d2fd264948f4943816a717
Murat Sahin <yazilimogren1111@gmail.com>
2021-06-12 14:56:53 +0200

    admin panel is deleted

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -3,10 +3,7 @@ import React, { useContext } from 'react';
 import Detail from "../components/Detail/Detail";
 import Footer from "../components/Footer/Footer";
 import Main from "../components/Main/Main";
-import PostCreate from "../components/Signin-up/PostCreate";
-import Signin from "../components/Signin-up/Signin";
 import { AuthContext } from '../App'
-import Admin from "../components/Admin/Admin";
 import PostDetail from "../components/PostDetail/PostDetail";
 
 const AppRouter = () => {
@@ -16,10 +13,7 @@ const AppRouter = () => {
         <Router>
             <Switch>
                 <Route exact path='/' component={Main} />
-                <Route exact path='/admin' component={Admin} />
                 <Route exact path='/makale/:slug' component={PostDetail} />
-                <Route exact path='/signin' component={Signin} />
-                <Route exact path='/create' component={token ? PostCreate : Signin} />
                 <Route exact path='/detail/:area' component={Detail} />
             </Switch>
             <Footer />
