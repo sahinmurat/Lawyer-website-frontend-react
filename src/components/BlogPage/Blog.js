e7fc8e662c07b8bd820f3a3a8008ab6828ee13e1
Murat Sahin <yazilimogren1111@gmail.com>
2021-06-15 23:50:02 +0200

    circular progress added

--- src/components/BlogPage/Blog.js
+++ src/components/BlogPage/Blog.js
@@ -1,12 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import LinearProgress from '@material-ui/core/LinearProgress'
 
 import './BlogStyle.css'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      '& > * + *': {
+        marginLeft: theme.spacing(2),
+      },
+    },
+  }));
 
 function Blog() {
     const [list, setList] = useState([])
     const history = useHistory();
+    const classes = useStyles();
+  
 
     useEffect(() => {
         fetchData()
@@ -21,13 +35,24 @@ function Blog() {
     return (
         <div>
             <h2 class="blog-title "> En Son Eklenenler </h2>
-            {
+{list ? list.map((item, index) => {
+                    return(
+                        <h5 class="post-title" key= {index} onClick={() => history.push(`/makale/${item.title}`, { params: { item } })} >{item.title}</h5>
+                    )
+                }) : <div className={classes.root}>
+                        <LinearProgress color="secondary" />
+                        <CircularProgress color="secondary" />
+                        <LinearProgress color="secondary" />
+                    </div>}
+
+
+            {/* {
                 list?.map((item, index) => {
                     return(
                         <h5 class="post-title" key= {index} onClick={() => history.push(`/makale/${item.title}`, { params: { item } })} >{item.title}</h5>
                     )
                 })
-            }
+            } */}
         </div>
     )
 }
