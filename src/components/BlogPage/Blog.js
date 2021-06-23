1211fd0dc943c9e2f32c9b394f76bbef56cf7994
Murat Sahin <yazilimogren1111@gmail.com>
2021-06-23 23:48:49 +0200

    blog page is styled

--- src/components/BlogPage/Blog.js
+++ src/components/BlogPage/Blog.js
@@ -9,22 +9,22 @@ import './BlogStyle.css'
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      textAlign:'center',
-      '& > * + *': {
-        marginLeft: theme.spacing(2),
-      },
+        textAlign: 'center',
+        '& > * + *': {
+            marginLeft: theme.spacing(2),
+        },
     },
-    circular : {
-        textAlign:'center',
-        backgroundColor:'#191970'
+    circular: {
+        textAlign: 'center',
+        backgroundColor: '#191970'
     }
-  }));
+}));
 
 function Blog() {
     const [list, setList] = useState([])
     const history = useHistory();
     const classes = useStyles();
-  
+
 
     useEffect(() => {
         fetchData()
@@ -34,17 +34,22 @@ function Blog() {
         var result = await axios.get('https://emirback.herokuapp.com/list');
         setList(result.data.results);
     }
- console.log(list)
+    console.log(list)
     return (
         <div id='blogPage'>
-            <h2 className="blog-title "> En Son Eklenenler </h2>
-{list.length > 0 ? list.map((item, index) => {
-                    return(
-                        <h5 className="post-title" key= {index} onClick={() => history.push(`/makale/${item.title}`, { params: { item } })} >{item.title}</h5>
-                    )
-                }) : <div className={classes.root}>
-                        <CircularProgress className={classes.circular} color="secondary" />
-                    </div>}
+            <div className='backgroundEffeckt'>
+            </div>
+            <div className='blogText'>
+            <h2 className="blog-title "> GÜNCEL BiLGiLER </h2>
+            {list.length > 0 ? list.map((item, index) => {
+                return (
+                    <h5 className="post-title" key={index} onClick={() => history.push(`/makale/${item.title}`, { params: { item } })} >- {item.title}</h5>
+                )
+            }) : <div className={classes.root}>
+                <CircularProgress className={classes.circular} color="secondary" />
+            </div>}
+            </div>
+            
 
         </div>
     )
